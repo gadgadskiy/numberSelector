4dbdcfea7c57f7be217eb6eac014a54db37481cc
Sergiy Onkov <sonkov@playtika.com>
2016-10-27 15:09:04 +0300

    initial commit

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Calculator {
+	public double calculate(String input) {
+		List<String> numberStrings = Arrays.asList(input.split("[+\\-*\\/]"));
+		List<Double> numbers = new ArrayList<>();
+		numberStrings.forEach(s -> numbers.add(Double.parseDouble(s)));
+		ArrayList<String> signs = new ArrayList<>(Arrays.asList(input.split("\\d+")));
+		signs.remove(0);
+		int length = signs.size();
+		for (int i = 0; i < length; i++) {
+			if ("*/".contains(signs.get(i))) {
+				numbers.set(i, doHighMath(numbers.get(i), numbers.get(i + 1), signs.remove(i)));
+				numbers.remove(i + 1);
+				length--;
+				i--;
+			}
+		}
+		length = signs.size();
+		for (int i = 0; i < length; i++) {
+			if ("+-".contains(signs.get(i))) {
+				numbers.set(i, doLowMath(numbers.get(i), numbers.get(i + 1), signs.remove(i)));
+				numbers.remove(i + 1);
+				length--;
+				i--;
+			}
+		}
+		return numbers.get(0);
+	}
+
+	private double doHighMath(double first, double second, String sign) {
+		switch (sign) {
+			case "*": return first * second;
+			case "/": return first / second;
+			default: throw new IllegalArgumentException("Unknown sign " + sign);
+		}
+	}
+
+	private double doLowMath(double first, double second, String sign) {
+		switch (sign) {
+			case "+": return first + second;
+			case "-": return first - second;
+			default: throw new IllegalArgumentException("Unknown sign " + sign);
+		}
+	}
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+    private final Calculator calculator = new Calculator();
+
+    public static void main(String[] args) {
+	    Main main = new Main();
+        String s1 = "11+2*3"; //17
+        String s2 = "11+2/3"; //11.666
+        String s3 = "11+2/3/4"; //11.166
+        String s4 = "11+2/3*3"; //13
+        String s5 = "1-4/3+2/5"; //0.066
+        String s6 = "1/3-2*8+1/3"; //-15.333
+        String s7 = "1/3-2*8-1/3"; //-16
+        System.out.println(main.calculator.calculate(s1));
+        System.out.println(main.calculator.calculate(s2));
+        System.out.println(main.calculator.calculate(s3));
+        System.out.println(main.calculator.calculate(s4));
+        System.out.println(main.calculator.calculate(s5));
+        System.out.println(main.calculator.calculate(s6));
+        System.out.println(main.calculator.calculate(s7));
+    }
+}
