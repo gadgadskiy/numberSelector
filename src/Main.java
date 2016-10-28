49131c7a6f6cc80ae644a78783d4e5b44b2d06bd
Sergiy Onkov <sonkov@playtika.com>
2016-10-28 16:38:38 +0300

    added todo list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 import java.util.List;
 
+/**
+ * TODO list:
+ * user API for input (input max 11 symbols, result - ?)
+ * optimize solutions 4520004 => 4*5*20/004, 4+5*20+00-4 etc
+ * processing ()
+ * unit tests
+ * fix -1*2*3
+ * fix -1*-2*-3
+ */
 public class Main {
     private final Calculator calculator = new Calculator();
     private final SignFiller signFiller = new SignFiller();
@@ -19,7 +28,7 @@ public class Main {
         main.doSelection("345676235", 100);
         main.doSelection("3456762351", 100);
         main.doSelection("29385723045", 100);
-        main.doSelection("4520004", 100); //TODO: optimize solutions 4*5*20/004, 4+5*20+00-4 etc
+        main.doSelection("4520004", 100);
         System.out.println("---------------------------------");
     }
 
@@ -27,14 +36,13 @@ public class Main {
         System.out.println("---------------------------------");
         System.out.println("try find solutions for: " + inputString + " => " + expectedResult);
         List<String> variants = signFiller.fillWithSimpleSigns(inputString);
-        int[] successNumber = {1};
+        int[] successNumber = {0};
         variants.forEach(variant -> {
             if (calculator.calculate(variant) == expectedResult) {
-                System.out.println(successNumber[0] + ") " + variant + " = " + expectedResult);
-                successNumber[0]++;
+                System.out.println(++successNumber[0] + ") " + variant + " = " + expectedResult);
             }
         });
-        if (successNumber[0] == 1) {
+        if (successNumber[0] == 0) {
             System.out.println("sorry, we didn't find solutions");
         }
     }
